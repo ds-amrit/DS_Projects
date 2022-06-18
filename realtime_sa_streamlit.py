667c8042694bb76bf2e6d6db5b00feae40b60a67
ds-amrit <107737679+ds-amrit@users.noreply.github.com>
2022-06-17 23:14:01 -0400

    Update realtime_sa_streamlit.py
    Added comments

--- realtime_sa_streamlit.py
+++ realtime_sa_streamlit.py
@@ -6,15 +6,23 @@ Created on Fri Jun 17 20:20:56 2022
 """
 
 
-
+# IMPORTING LIBRARIES
 import streamlit as st
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 st.write("# Real Time Sentiment Analysis")
 
+# Accepting user input
 user_input = st.text_input("Enter text to analyze it's sentiment >>: ")
+
+# Downloading VADER LEXICON
 nltk.download("vader_lexicon")
+
+# Initializing Sentiment Analysis Model
 sia = SentimentIntensityAnalyzer()
+
+# Calculating the sentiment score for the input
 score = sia.polarity_scores(user_input)
 
 if score == 0:
