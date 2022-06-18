e19c459483a2cc41017b06a1ae06a7b953f4c913
ds-amrit <107737679+ds-amrit@users.noreply.github.com>
2022-06-17 23:10:39 -0400

    Add files via upload
    In this project, I have performed Real-time sentiment analysis using the VADER sentiment lexicon and deployed the model using a streamlit framework.

---
+++ realtime_sa_streamlit.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 17 20:20:56 2022
+
+@author: amrit
+"""
+
+
+
+import streamlit as st
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+st.write("# Real Time Sentiment Analysis")
+
+user_input = st.text_input("Enter text to analyze it's sentiment >>: ")
+nltk.download("vader_lexicon")
+sia = SentimentIntensityAnalyzer()
+score = sia.polarity_scores(user_input)
+
+if score == 0:
+    st.write(" ")
+elif score["neg"] != 0:
+    st.write("# Negative")
+elif score["pos"] != 0:
+    st.write("# Positive")
